70f2349d5832b4e2069783ac5121734fc7a29518
fadhil <fadhildarma13@gmail.com>
2020-12-01 20:18:55 +0700

    state count

--- src/App.js
+++ src/App.js
@@ -1,61 +1,24 @@
+import { useState } from "react";
+
 import Header from "./components/Header";
 import Layout from "./components/Layout";
 
 function App() {
-  //penggunaan var
-  var title = "Hello World";
-  var title = "Title Baru";
-
-  //penggunaan let
-  let greeting = "Selamat";
-  let day = "Night";
-
-  if (day === "Night") {
-    greeting = greeting + " Malam";
-  } else {
-    greeting = greeting + " Pagi";
-  }
-
-  //penggunaan const
-  const appName = "Belajar React JS";
-
-  //function
-  // function printHelloWorld() {
-  //   return "hello world";
-  // }
-
-  //return jsx menggunakan arrow function
-  const printHelloWorld = (params) => {
-    return (
-      <>
-        <h1>{params}</h1>
-        <h2>Selamat Pagi</h2>
-      </>
-    );
-  };
-
-  //function yang dijalankan ketika event onclick dijalankan
-  function sumNumber(number1, number2) {
-    console.log(number1 + number2);
-  }
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <Header />
       <Layout>
-        <h1>{title}</h1>
-        <h1>{greeting}</h1>
-        <h1>{appName}</h1>
-        <h1>{printHelloWorld("Hello World")}</h1>
-        <button onClick={() => sumNumber(1, 2)}>Click Me</button>
+        <button onClick={() => setCount(count + 1)}>Increament</button>
+        <h1>Count: {count}</h1>
+        <button onClick={() => setCount(count - 1)}>Decreament</button>
       </Layout>
       <Footer />
     </>
   );
 }
-
 function Footer() {
   return <h1>Ini adalah footer</h1>;
 }
-
 export default App;
