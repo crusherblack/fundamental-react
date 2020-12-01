58ccfc62e7dcaf5b2afb36c24a73ad81dd0b1423
fadhil <fadhildarma13@gmail.com>
2020-12-01 22:05:51 +0700

    react styling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import { useState } from "react";
 import Header from "./components/Header";
 import Layout from "./components/Layout";
 
+//import Dropdown
+import { Dropdown } from "react-bootstrap";
+
+//css
+import "./app-style.css";
+
+//import bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
   const [count, setCount] = useState(0);
 
@@ -31,9 +40,49 @@ function App() {
     <>
       <Header {...props} />
       <Layout>
-        <button onClick={() => setCount(count + 1)}>Increament</button>
-        <h1>Count: {count}</h1>
-        <button onClick={() => setCount(count - 1)}>Decreament</button>
+        {/* classname */}
+        <button className="btn-secondary" onClick={() => setCount(count + 1)}>
+          Increament
+        </button>
+        {/* bootstrap */}
+        <button className="btn btn-primary" onClick={() => setCount(count + 1)}>
+          Increament
+        </button>
+        <button
+          style={styles.buttonPrimary}
+          onClick={() => setCount(count + 1)}
+        >
+          Increament
+        </button>
+        {/* inline styling */}
+
+        <h1
+          style={{
+            backgroundColor: "red",
+            fontSize: "18px",
+            color: "white",
+          }}
+        >
+          Count: {count}
+        </h1>
+        <button
+          style={styles.buttonPrimary}
+          onClick={() => setCount(count - 1)}
+        >
+          Decreament
+        </button>
+        {/* Dropdown react-bootstrap */}
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            Dropdown Button
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
       </Layout>
       <Footer />
     </>
@@ -45,3 +94,16 @@ function Footer() {
 }
 
 export default App;
+
+/* Reusable Styles */
+
+const styles = {
+  buttonPrimary: {
+    backgroundColor: "red",
+    padding: "10px 20px",
+    borderColor: "red",
+    borderRadius: "5px",
+    color: "white",
+    fontSize: "18px",
+  },
+};
